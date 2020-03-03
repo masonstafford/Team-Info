465cee3b805211d86079c1fd486e8ec53cbd7701
Mason Stafford <masonstafford00@gmail.com>
2020-03-03 09:44:48 -0500

    passed all tests, fixing app.js

--- Develop/app.js
+++ Develop/app.js
@@ -14,105 +14,142 @@ const render = require('./lib/htmlRenderer');
 async function start() {
     console.log("Please build your team");
 
-    var myTeam = [];
-    var myTeamNumberSize;
+    const myTeam = [];
 
-    await inquirer.prompt(
+   await inquirer.prompt([
         {
-            type: "number",
-            message: "How many people are in your team?",
-            name: "noOfTeamMem"
+            type: "input",
+            message: `What is you manager's first name?`,
+            name: "name"
+        },
+        {
+            type: "input",
+            message: `What is you manager's id?`,
+            name: "id"
+        },
+        {
+            type: "input",
+            message: `What is you manager's Email?`,
+            name: "email"
+        },
+        {
+            type: "input",
+            message: `What is you manager's office number?`,
+            name: "email"
+        },
+        {
+            type: "list",
+            message: `Which type of team member would you like to add?`,
+            name: "title",
+            choices: ["Engineer", "Intern", "Done"]
         }
-    ).then((data) => {
-        myTeamNumberSize = data.noOfTeamMem + 1;
+    ]).then((data) => {
+        const intern = new Intern(name, id, email, data.school);
+        teamMember = fs.readFileSync("templates/intern.html");
+        myTeam = myTeam + "\n" + eval('`' + teamMember + '`');
     });
 
-    if (myTeamNumberSize === 0) {
-        console.log("Hmmmmm, no team then? Ok!");
-        return;
+    switch (title) {
+
+        case "Intern":
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your intern's name?",
+                    name: "name"
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's id?",
+                    name: "id"
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's email?",
+                    name: "email"
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's school?",
+                    name: "school"
+                },
+                {
+                    type: "list",
+                    message: `Which type of team member would you like to add?`,
+                    name: "title",
+                    choices: ["Engineer", "Intern", "Done"]
+                }
+            ]).then((data) => {
+                const intern = new Intern(name, id, email, data.school);
+                teamMember = fs.readFileSync("templates/intern.html");
+                myTeam = myTeam + "\n" + eval('`' + teamMember + '`');
+            }); break;
+
+        case "Engineer":
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your engineer's name?",
+                    name: "name"
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's id?",
+                    name: "id"
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's email?",
+                    name: "email"
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's GitHub?",
+                    name: "github"
+                },
+                {
+                    type: "list",
+                    message: `Which type of team member would you like to add?`,
+                    name: "title",
+                    choices: ["Engineer", "Intern", "Done"]
+                }
+            ]).then((data) => {
+                const engineer = new Engineer(name, id, email, data.github);
+                teamMember = fs.readFileSync("templates/engineer.html");
+                myTeam = myTeam + "\n" + eval('`' + teamMember + '`');
+            }); break;
     }
-
-    for (i = 1; i < myTeamNumberSize; i++) {
-        let name;
-        let id;
-        let title;
-        let email;
-
-        await inquirer.prompt([
-            {
-                type: "input",
-                message: `Please enter employee (${i})'s name?`,
-                name: "name"
-            },
-            {
-                type: "input",
-                message: `Please enter employee (${i})'s id?`,
-                name: "id"
-            },
-            {
-                type: "input",
-                message: `Please enter employee (${i})'s Email?`,
-                name: "email"
-            },
-            {
-                type: "list",
-                message: `Please enter employee (${i})'s job role?`,
-                name: "title",
-                choices: ["Engineer", "Intern", "Manager"]
-            }
-        ]).then((data) => {
-            name = data.name;
-            id = data.id;
-            title = data.title;
-            email = data.email;
-        });
-
-        switch (title) {
-            case "Manager":
-                await inquirer.prompt([
-                    {
-                        type: "input",
-                        message: "Please enter the Manager's office number?",
-                        name: "officeNo"
-                    }
-                ]).then((data) => {
-                    const manager = new Manager(name, id, email, data.officeNo);
-                    teamMember = fs.readFileSync("templates/manager.html");
-                    myTeam = myTeam + "\n" + eval('`' + teamMember + '`');
-                }); break;
-
-            case "Intern":
-                await inquirer.prompt([
-                    {
-                        type: "input",
-                        message: "Please enter the interns school",
-                        name: "school"
-                    }
-                ]).then((data) => {
-                    const intern = new Intern(name, id, email, data.school);
-                    teamMember = fs.readFileSync("templates/intern.html");
-                    myTeam = myTeam + "\n" + eval('`' + teamMember + '`');
-                }); break;
-
-            case "Engineer":
-                await inquirer.prompt([
-                    {
-                        type: "input",
-                        message: "Please enter the engineer's GitHub?",
-                        name: "github"
-                    }
-                ]).then((data) => {
-                    const engineer = new Engineer(name, id, email, data.github);
-                    teamMember = fs.readFileSync("templates/engineer.html");
-                    myTeam = myTeam + "\n" + eval('`' + teamMember + '`');
-                }); break;
-        }
-    }
-    fs.writeFile(outputPath, render(myTeam), function (err) {
-        console.log(myTeam)
-        if (err) {
-            return console.log(err);
-        }
-    });
 }
+fs.writeFile(outputPath, render(myTeam), function (err) {
+    console.log(myTeam)
+    if (err) {
+        return console.log(err);
+    }
+});
 start();
+
+
+
+
+    // var myTeamNumberSize;
+
+    // inquirer.prompt(
+    //     {
+    //         type: "number",
+    //         message: "How many people are in your team?",
+    //         name: "noOfTeamMem"
+    //     }
+    // ).then((data) => {
+    //     myTeamNumberSize = data.noOfTeamMem + 1;
+    // });
+
+    // if (myTeamNumberSize === 0) {
+    //     console.log("Hmmmmm, no team then? Ok!");
+    //     return;
+    // }
+
+    // for (i = 1; i < myTeamNumberSize; i++) {
+    //     let name;
+    //     let id;
+    //     let title;
+    //     let email;
